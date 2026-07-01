3b6f278f3443af5f60e9f604cc357bf245dfb2cb
OnkarNanaware <154796622+OnkarNanaware@users.noreply.github.com>
2026-07-01 22:30:25 +0530

    Add Game with Integers [A] from Codeforces

---
+++ 1899/A - Game with Integers/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+ 
+int main()
+{
+    int tc;
+    cin >> tc;
+    while(tc--)
+    {
+        int n;
+        cin>>n;
+        if(n%3==0)
+        {
+            cout<<"Second"<<endl;
+ 
+        }
+        else
+        {
+            cout<<"First"<<endl;
+        }
+    }
+    return 0;
+}
