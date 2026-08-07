123e4aacd1a8aecf2406faf61e280cddae3c0fcb
OnkarNanaware <154796622+OnkarNanaware@users.noreply.github.com>
2026-08-07 22:17:34 +0530

    Add Make AP [B] from Codeforces

---
+++ 1624/B - Make AP/solution.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+    long long a, b, c;
+    cin >> a >> b >> c;
+    bool answer=false;
+    long long new_a=2*b-c;
+    if(new_a>0 && new_a%a==0) answer=true;
+    long long new_b=(a+c)/2;
+    if((a+c)%2==0 && new_b>0 && new_b%b==0) answer=true;
+    long long new_c=2*b-a;
+    if(new_c>0 && new_c%c==0) answer=true;
+ 
+if(answer)cout<<"YES
+";
+else cout<<"NO
+";
+    
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
