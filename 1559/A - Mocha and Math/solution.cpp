ab03ddb8fad04ff5c26590da4bfec48448871a51
OnkarNanaware <154796622+OnkarNanaware@users.noreply.github.com>
2026-08-11 00:52:31 +0530

    Add Mocha and Math [A] from Codeforces

---
+++ 1559/A - Mocha and Math/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin>>t;
+    while (t--) {
+        int n;
+        cin>>n;
+        long long a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+            
+        }
+        long long ele=a[0];
+        for(int i=0;i<n;i++)
+        {
+            ele=(ele&a[i]);
+        }
+        cout<<ele<<endl;
+        
+        
+    }
+    return 0;
+}
